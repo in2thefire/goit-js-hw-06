57d177ab9541e9f2350c356beb4ba1c3a94b1ba1
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-15 19:50:27 +0200

    ghpages

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,5 @@
 const list = document.querySelector("#categories");
 console.log(`Number of categories: ${list.children.length}`);
-const item = document.querySelector(".item");
 [...list.children].forEach((item) => {
   console.log(
     `Category: ${item.firstElementChild.textContent}\nElements: ${item.lastElementChild.children.length}`
