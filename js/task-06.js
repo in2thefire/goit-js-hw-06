a44aa3e411a8b9c13963ba01b8e561cd7de69c7a
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-21 20:25:33 +0200

    06

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,15 @@
 const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", onBlur);
+
 function onBlur(evt) {
   const currentLength = evt.currentTarget.value.length;
-  const requiredLength = inputEl.dataset.length;
+  const requiredLength = Number(inputEl.dataset.length);
+
   if (currentLength === requiredLength) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 }
