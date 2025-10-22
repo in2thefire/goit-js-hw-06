f70c1ac997ffb0c8fafc10fa363a2c2383f6e50c
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-22 17:13:11 +0200

    09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const bodyEl = document.body;
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onClick);
+
+function onClick() {
+  const recentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = recentColor;
+  spanEl.textContent = recentColor;
+}
